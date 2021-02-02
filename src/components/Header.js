dabb33c892ce3a23baeb927bc99652c590a0bf61
Alexander Shvyrev <shurik_stv@yahoo.com>
2021-02-02 16:22:47 -0500

    a simple tasks manager using react. react hooks and json-server

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
+import Button from './Button'
+
+
+const Header = ({ title, onAdd, showForm }) => {
+    const location = useLocation()
+    return (
+        <header className="header">
+            <h1>{title}</h1>
+            {location.pathname === '/' &&
+                <Button
+                    color={showForm ? 'red' : 'green'}
+                    text={showForm ? 'Hide' : 'Add'}
+                    onClick={onAdd}
+                />}
+
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Tasks Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+export default Header
